885a856c4637752bdfa67b866df45fedd6865e05
Sasha Tatar <sasa.tatar@gmail.com>
2020-07-25 19:17:25 +0200

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
+
 const Application: React.FC<{}> = () => {
+
+    const [query, setQuery] = useState('');
+
     return (
         <div>
-            <div className="min-h-screen flex items-center justify-center">
+            <div className="min-h-screen flex flex-col items-center justify-center">
                 <h1 className="text-5xl text-blue-500 font-sans">Hello world!</h1>
+                <input
+                    className="p-2 m-1 border-gray-400 border w-1/6"
+                    type="text"
+                    value={query}
+                    onChange={(e) => {
+                        setQuery(e.target.value);
+                    }}
+                    placeholder="GitHub username"
+                />
+                <button className="btn btn-blue">Search</button>
             </div>
-            <button className="btn btn-blue rounded-none">Test</button>
         </div>
     )
 };
