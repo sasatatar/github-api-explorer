551cb72167518b70aefb5d39b3a7be15160395e4
Sasha Tatar <sasa.tatar@gmail.com>
2020-07-30 20:38:55 +0200

    Fix SearchBox and search history

--- src/SearchBox.tsx
+++ src/SearchBox.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useLazyQuery, gql } from '@apollo/client';
+import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
 import { User, UserVariables, User_user } from '~/__generated__/User';
 import { Link } from 'react-router-dom';
 import { useDebounce } from '~/util/useDebounce';
 import { useLocalStorage } from '~/util/useLocalStorage';
-import { Icon } from '~components/Icon';
 
 const USER_QUERY = gql`
     query User ($login: String!) { 
@@ -18,29 +17,16 @@ const USER_QUERY = gql`
     }
 `;
 
-
 export const SearchBox: React.FC<{}> = () => {
 
-    const [login, setLogin] = useState('');
-    let debouncedLogin = useDebounce(login, 500);
-    let [isFocused, setIsFocused] = useState(false);
+    const [query, setQuery] = useState('');
+    let login = useDebounce(query, 500);
+    let [isOpen, setIsOpen] = useState(false);
     let [searchHistory, updateSearchHistory] = useSearchHistory();
-
-    const [getUser, { loading, data }] = useLazyQuery<User, UserVariables>(USER_QUERY);
-
-    let [user, setUser] = useState(data?.user);
-    if (user) setIsFocused(true);
-
-    useEffect(() => {
-        if (!debouncedLogin) return;
-        getUser({ variables: { login: debouncedLogin } });
-    }, [debouncedLogin]);
 
     function onSelectUser(user: User_user) {
         updateSearchHistory(user);
-        setUser(null);
-        setIsFocused(false);
-        setLogin('');
+        setIsOpen(false);
     }
 
     return (
@@ -54,35 +40,29 @@ export const SearchBox: React.FC<{}> = () => {
                             <input type="text"
                                 className="text-lg outline-none w-full"
                                 placeholder="GitHub username"
-                                value={login}
-                                onChange={(e) => setLogin(e.target.value)}
-                                //${isFocused ? '' : 'border-opacity-0'}
-                                // onFocus={(e) => {
-                                //     setIsFocused(true);
-                                // }}
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                                //${isOpen ? '' : 'border-opacity-0'}
+                                onFocus={(e) => {
+                                    setIsOpen(true);
+                                }}
                                 onBlur={(e) => {
-                                    setTimeout(() => setIsFocused(false), 500);
+                                    setTimeout(() => setIsOpen(false), 300);
                                 }}
                             />
-                            {/* <LoadingSpinner /> */}
                             <div className="ml-2">
-                                {
-                                    loading
-                                        ? <LoadingSpinner className="h-5 w-5" />
-                                        : <i className={`fas ${login ? 'fa-times' : 'fa-search'} w-5 text-lg text-gray-400 text-center hover:text-gray-600 lineheight-unset`}
-                                            onClick={() => {
-                                                setLogin('');
-                                                setUser(null);
-                                                setIsFocused(false);
-                                            }}
-                                        />
-                                }
+                                <i className={`fas ${query ? 'fa-times' : 'fa-search'} w-5 text-lg font-light text-gray-400 text-center hover:text-gray-600 lineheight-unset`}
+                                    onClick={() => {
+                                        setQuery('');
+                                        setIsOpen(false);
+                                    }}
+                                />
                             </div>
                         </div>
                         {
-                            isFocused && (
-                                user
-                                    ? <SearchResult user={data?.user || null} onSelectUser={onSelectUser} />
+                            isOpen && (
+                                login
+                                    ? <SearchResult login={login} onSelectUser={onSelectUser} />
                                     : <SearchHistory searchHistory={searchHistory} onSelectUser={onSelectUser} />
                             )
                         }
@@ -102,10 +82,6 @@ const LoadingSpinner: React.FC<{ className?: string }> = ({ className }) => {
         <svg className={`circle animate-spin ${className}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
             <circle cx="50" cy="50" r="45" />
         </svg>
-        // <svg className={`animate-spin text-gray-600 ${className}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        //     {/* <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle> */}
-        //     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        // </svg>
     )
 }
 
@@ -117,57 +93,66 @@ const SearchHistory: React.FC<{
         <div className={`absolute w-1/2 h-1/2 shadow-md border rounded-md bg-white transition-opacity`}>
             <div className="p-5">
                 <h1 className="text-xl mb-3">Search history</h1>
-                <React.Fragment>
-                    {
-                        searchHistory.length > 0
-                        && (
-                            <ul>
-                                <React.Fragment>
-                                    {searchHistory.map((user: User_user) => (
-                                        <li
-                                            onClick={() => onSelectUser(user)}
-                                        >
-                                            <img src={user.avatarUrl} alt={user.name || ''} className="m-2" />
-                                            <div>{user.name}</div>
-                                            <span>{user.login}</span>
-                                            <div>{user.email}</div>
-                                            <a href={user.url}>Go to github page</a>
-                                        </li>
-                                    ))}
-                                </React.Fragment>
-                            </ul>
-                        )
-                    }
-                </React.Fragment>
+                {
+                    searchHistory.length > 0
+                    && (
+                        <ul>
+                            <React.Fragment>
+                                {searchHistory.map((user: User_user) => (
+                                    <li
+                                        onClick={() => onSelectUser(user)}
+                                        key={user.login}
+                                    >
+                                        <img src={user.avatarUrl} alt={user.name || ''} className="m-2" />
+                                        <div>{user.name}</div>
+                                        <span>{user.login}</span>
+                                        <div>{user.email}</div>
+                                        <a href={user.url}>Go to github page</a>
+                                    </li>
+                                ))}
+                            </React.Fragment>
+                        </ul>
+                    )
+                }
             </div>
         </div>
     )
 }
 
 const SearchResult: React.FC<{
-    user: User_user | null,
+    login: string,
     onSelectUser: (user: User_user) => void
-}> = ({ user, onSelectUser }) => {
+}> = ({ login, onSelectUser }) => {
+
+    let { loading, error, data } = useQuery<User, UserVariables>(USER_QUERY, { variables: { login } });
+    // const [getUser, { loading, error, data }] = useLazyQuery<User, UserVariables>(USER_QUERY);
+    if (error) console.error(error);
+
+    let user = data?.user;
 
     return (
         <div className={`absolute w-1/2 max-h-1/2 shadow-md border rounded-md bg-white transition-opacity`}>
             <div className="p-5">
                 {
-                    user
+                    loading
                         ? (
-                            <div
-                                onClick={() => {
-                                    onSelectUser(user);
-                                }}
-                            >
-                                <img src={user.avatarUrl} />
-                                <div>{user.name}</div>
-                                <span>{user.login}</span>
-                                <div>{user.email}</div>
-                                <a href={user.url}>Go to github page</a>
-                            </div>
+                            <div><LoadingSpinner /></div>
                         )
-                        : <div>No matches</div>
+                        : user
+                            ? (
+                                <div
+                                    onClick={() => {
+                                        user && onSelectUser(user);
+                                    }}
+                                >
+                                    <img src={user.avatarUrl} />
+                                    <div>{user.name}</div>
+                                    <span>{user.login}</span>
+                                    <div>{user.email}</div>
+                                    <a href={user.url}>Go to github page</a>
+                                </div>
+                            )
+                            : <div>No matches found</div>
                 }
             </div>
         </div>
@@ -182,7 +167,7 @@ function useSearchHistory() {
         searchHistory,
         (user: User_user) => {
             let newSearchHistory = searchHistory.filter((u: User_user) => u.login != user.login);
-            newSearchHistory = [user, ...newSearchHistory].slice(10);
+            newSearchHistory = [user, ...newSearchHistory].slice(0, 10);
 
             if (searchHistory != newSearchHistory) {
                 saveSearchHistory(newSearchHistory);
